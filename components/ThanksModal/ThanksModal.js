32a7d5ba1bd23b2702d518dcb60bb40924891485
gloria-tampuri <gloriatampuri15@gmail.com>
2022-09-09 12:28:17 +0000

    modal fix

--- components/ThanksModal/ThanksModal.js
+++ components/ThanksModal/ThanksModal.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import classes from './ThanksModal.module.css'
+// import Layout from '../Layout/Layout'
 
 const ThanksModal = (props) => {
   return (
-    <div >
+   
         <div className={classes.backdrop}>
             <div className={classes.wrap} >
-                <p> Thanks for your question. Looking forward to seeing you on <span>11th September</span></p>
+                <p> Thanks for your question. Looking forward to seeing you on <span>18th September</span></p>
                 <button onClick={props.closeMsg}> Okay </button>
             </div>
         </div>
-    </div>
+    
   )
 }
 
